ea4c34ed113b2a4e376a0877319ead06f629027e
“Chandu” <Chandubopche321@gmail.com>
2023-09-18 08:51:56 +0530

    contact form added ,Serving HTML , CSS and JS

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set the views directory and view engine (EJS in this case)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); // You can use a different view engine if preferred
 
 // Store messages in an array (for simplicity; in production, you'd use a database)
 const messages = [];
@@ -12,11 +17,11 @@ const messages = [];
 const users = {};
 
 // Serve static files (CSS, JavaScript, etc.)
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Set up a route to serve the HTML page
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 // API endpoint to log in with a username
@@ -47,6 +52,27 @@ app.post('/send', (req, res) => {
 // API endpoint to fetch messages
 app.get('/messages', (req, res) => {
   res.json(messages);
+});
+
+// Set up a route to serve the Contact Us page
+app.get('/contactus', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contactus.html'));
+});
+
+// Handle the Contact Us form submission
+app.post('/success', (req, res) => {
+  const { name, email } = req.body;
+  if (name && email) {
+    // You can perform additional actions here, such as sending an email
+    res.send('Form successfully filled');
+  } else {
+    res.status(400).send('Name and email are required');
+  }
+});
+
+// Handle 404 errors with a custom "Page not found" view
+app.use((req, res) => {
+  res.status(404).render('not-found'); // Render the not-found view
 });
 
 app.listen(port, () => {
